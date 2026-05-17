0a8714c530c1eeb06cc767f722648fa8fd6c006d
zaytlily <1nastyakotya@gmail.com>
2026-05-17 19:53:39 +0700

    Add client shipments module

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -6,6 +6,7 @@ from .users import router as users_router
 from .transport_categories import router as transport_categories_router
 from .transports import router as transports_router
 from .delivery_services import router as delivery_services_router
+from .client_shipments import router as client_shipments_router
 
 router = APIRouter(prefix=settings.url.prefix)
 
@@ -14,3 +15,4 @@ router.include_router(users_router)
 router.include_router(transport_categories_router)
 router.include_router(transports_router)
 router.include_router(delivery_services_router)
+router.include_router(client_shipments_router)
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -5,5 +5,6 @@ from .user import User
 from .transport_category import TransportCategory
 from .transport import Transport
 from .delivery_service import DeliveryService
+from .client_shipment import ClientShipment
 
-__all__ = ["Base", "db_helper", "Role", "User", "TransportCategory", "Transport", "DeliveryService"]
+__all__ = ["Base", "db_helper", "Role", "User", "TransportCategory", "Transport", "DeliveryService", "ClientShipment"]
